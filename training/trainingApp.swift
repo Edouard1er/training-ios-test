17089ec67b72f0a2e35d6edbc538bc7f6cd67c3a
Edouard Chevenslove <edouardchevenslove98@gmail.com>
2023-12-05 14:38:15 +0100

    Initial commit

---
+++ training/trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  trainingApp.swift
+//  training
+//
+//  Created by EDOUARD CHEVENSLOVE on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
